3ddf02fac4db4b6a9cedae4a54cfb712ba925656
saikiran520 <saikiran@ebanitech.com>
2026-03-10 15:49:08 +0530

    push

---
+++ android/app/src/main/kotlin/com/ebani/faceidentification/faceidentification/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.ebani.faceidentification.faceidentification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Currently, all face recognition logic has been moved to the Dart side (FaceService.dart)
+    // for better cross-platform support and easier maintenance.
+}
